9b3d1b164e0d11716fc56ea12652dc595224a38f
Feher Peter <feher.peter@innobyte.hu>
2018-06-08 16:49:44 +0200

    .

--- src/main/webapp/app/entities/order-entity/order-entity-detail.component.ts
+++ src/main/webapp/app/entities/order-entity/order-entity-detail.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpResponse } from '@angular/common/http';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import { Subscription } from 'rxjs/Subscription';
-import { JhiEventManager } from 'ng-jhipster';
+import {JhiAlertService, JhiEventManager} from 'ng-jhipster';
 
 import { OrderEntity } from './order-entity.model';
 import { OrderEntityService } from './order-entity.service';
 import {QueryConstants} from '../../shared/constants/query.constants';
+import {OrderItem} from '../order-item';
+import {OrderItemService} from '../order-item/order-item.service';
 
 @Component({
     selector: 'jhi-order-entity-detail',
@@ -15,12 +17,15 @@ import {QueryConstants} from '../../shared/constants/query.constants';
 export class OrderEntityDetailComponent implements OnInit, OnDestroy {
 
     orderEntity: OrderEntity;
+    orderItems: OrderItem[];
     private subscription: Subscription;
     private eventSubscriber: Subscription;
 
     constructor(
         private eventManager: JhiEventManager,
         private orderEntityService: OrderEntityService,
+        private orderItemService: OrderItemService,
+        private jhiAlertService: JhiAlertService,
         private route: ActivatedRoute
     ) {
     }
@@ -54,7 +59,23 @@ export class OrderEntityDetailComponent implements OnInit, OnDestroy {
         );
     }
 
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
+    }
+
     isFinalized(): boolean {
         return this.orderEntity.statusId === QueryConstants.orderStatus.LEZARVA;
+    }
+
+    onRaktarClick(id: number) {
+        this.orderItemService.query().subscribe((res: HttpResponse<OrderItem[]>) => {
+            this.orderItems = res.body;
+            this.orderItems = this.orderItems.filter((x) => x.orderEntityId === id);
+            this.orderEntityService.placeIntoProducts(this.orderItems, id).subscribe((response: HttpResponse<OrderEntity>) => {
+                this.jhiAlertService.success(
+                    'storageManagementApp.orderEntity.raktarbaFelveve', response.body.id, null);
+                this.eventManager.broadcast({ name: 'orderEntityListModification', content: 'OK'});
+            }, (response: HttpErrorResponse) => this.onError(response.message));
+        }, (res: HttpErrorResponse) => this.onError(res.message));
     }
 }
